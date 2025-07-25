063d3974aaa8d76c097b0ba9dcdf3df95d0624db
praveen gautam <gautampraveen351@gmail.com>
2025-07-25 16:55:33 +0530

    Add meta tags and favicon links to Head component for improved SEO and branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css"
 import Navigation from "@/components/navigation"
 import Footer from "@/components/footer"
 import FloatingContact from "@/components/floating-contact"
+import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -30,6 +31,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+       <Head>
+  <meta name="description" content="We help medtech teams clear regulatory hurdles and build scalable QMS—whether it’s hardware, software, or AI-driven innovation." />
+  <meta name="keywords" content="medical device, regulatory consulting, quality management, FDA, EU MDR, ISO 13485" />
+  <meta name="google-site-verification" content="McMVOQUvz4GIeWKwmJeSZO5NfoVZ2r27TV8ehLCpgR0" />
+  <link rel="icon" href="/favicon.png" />
+  <link rel="shortcut icon" href="/favicon.png" />
+</Head>
       <body className={inter.className}>
         <Navigation />
         <main>{children}</main>
